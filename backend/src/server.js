ebf78beef09f32f330aac082f060e9df0ef3d1f7
aarohi6364-commits <aarohi.6364@gmail.com>
2026-04-11 22:26:12 +0530

    :fire: signup implemented on the frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -15,6 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // req.body
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
