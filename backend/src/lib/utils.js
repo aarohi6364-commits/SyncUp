993bacca8d9e756e0d01f2205171515c5672c3ad
aarohi6364-commits <aarohi.6364@gmail.com>
2026-04-13 22:26:06 +0530

    :art: fixing backend

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -16,7 +16,7 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, // MS
         httpOnly: true, // prevent XSS attacks: cross-site scripting
-        sameSite: "strict", // CSRF attacks
+        sameSite: "None", // CSRF attacks
         secure: process.env.NODE_ENV === "development" ? false : true,
     });
 
